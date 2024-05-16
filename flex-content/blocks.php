d9d5de2827d719abdcd72167b7f866228118f240
Andrius_Gluzeckis <andrius@oneagency.co>
2024-05-16 12:08:00 +0100

    Stop work for now dispaly

--- flex-content/blocks.php
+++ flex-content/blocks.php
@@ -8,7 +8,8 @@ if (have_rows('content')):
         $fileName = sprintf('%s/flex-content/blocks/%s.php',  get_template_directory(), get_row_layout());
         
         if (file_exists($fileName)) : ?>
-            <?php get_template_part(sprintf('flex-content/blocks/%s', get_row_layout())); ?>
+        <h1>Stop for now</h1>
+            <?php // get_template_part(sprintf('flex-content/blocks/%s', get_row_layout())); ?>
         <?php else :
             get_template_part('404', null);
         endif;

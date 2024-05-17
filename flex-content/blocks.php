211fc8f4388bc8afdb1c8b80d863b7aa72139ceb
Andrius_Gluzeckis <andrius@oneagency.co>
2024-05-17 08:50:33 +0100

    Show progress

--- flex-content/blocks.php
+++ flex-content/blocks.php
@@ -8,8 +8,7 @@ if (have_rows('content')):
         $fileName = sprintf('%s/flex-content/blocks/%s.php',  get_template_directory(), get_row_layout());
         
         if (file_exists($fileName)) : ?>
-        <h1>Stop for now</h1>
-            <?php // get_template_part(sprintf('flex-content/blocks/%s', get_row_layout())); ?>
+            <?php get_template_part(sprintf('flex-content/blocks/%s', get_row_layout())); ?>
         <?php else :
             get_template_part('404', null);
         endif;
